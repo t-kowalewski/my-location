44daecd2f56c91671f03150f9a9c68e9cfd13612
t-kowalewski <wkowalewski@outlook.com>
2021-06-09 12:21:15 +0200

    add hide method - Modal class

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -8,19 +8,26 @@ export class Modal {
   show() {
     if ('content' in document.createElement('template')) {
       const modalElements = this.modalTemplateEl.content.cloneNode(true);
-      const backdropElement = modalElements.querySelector('.backdrop');
-      const modalElement = modalElements.querySelector('.modal');
+      this.backdropElement = modalElements.querySelector('.backdrop');
+      this.modalElement = modalElements.querySelector('.modal');
 
       const contentElement = this.contentTemplateEl.content.cloneNode(true);
 
-      modalElement.appendChild(contentElement);
+      this.modalElement.appendChild(contentElement);
 
-      document.body.insertAdjacentElement('afterbegin', modalElement);
-      document.body.insertAdjacentElement('afterbegin', backdropElement);
+      document.body.insertAdjacentElement('afterbegin', this.modalElement);
+      document.body.insertAdjacentElement('afterbegin', this.backdropElement);
     } else {
       alert(this.fallbackText);
     }
   }
 
-  hide() {}
+  hide() {
+    if (this.modalElement) {
+      document.body.removeChild(this.modalElement);
+      document.body.removeChild(this.backdropElement);
+      this.modalElement = null;
+      this.backdropElement = null;
+    }
+  }
 }
